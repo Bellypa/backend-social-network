d25ff63fe696520cde997af4d8af00c96bd17c48
Administrator <42242660+patricktigersoft@users.noreply.github.com>
2023-07-12 14:35:15 +0200

    initial commit

---
+++ src/main/java/com/backendsocialnetwork/BackendSocialNetworkApplication.java
@@ -0,0 +1,14 @@
+package com.backendsocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Devops using github options");
+		SpringApplication.run(BackendSocialNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendsocialnetwork/BackendSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendsocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
