111d73fbd4f11f58d9c67fc462faa8dd633499ed
Administrator <42242660+patricktigersoft@users.noreply.github.com>
2023-07-12 18:03:28 +0200

    pom configuration updates

--- src/main/java/com/backendsocialnetwork/BackendSocialNetworkApplication.java
+++ src/main/java/com/backendsocialnetwork/BackendSocialNetworkApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendSocialNetworkApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Devops using github options");
 		SpringApplication.run(BackendSocialNetworkApplication.class, args);
 	}
 
